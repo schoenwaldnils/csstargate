7e8a402f2eb141b4b0d6f36b9a94f926724a6e9a
Nils Schönwald <nils@schoenwald.media>
2016-07-22 17:03:54 +0200

    Work on js

--- source/components/stargate/stargate.js
+++ source/components/stargate/stargate.js
@@ -1,6 +1,48 @@
 class Stargate extends HTMLElement {
   createdCallback() {
-    // console.log(this);
+    // default options
+    this.options = {
+      address: [27, 7, 15, 32, 12, 30, 1],
+      delay: 4000,
+      selectors: {
+        symboles: '.Stargate-symboles',
+        symbole: '.Stargate-symbole',
+        eventHorizon: '.Stargate-horizon',
+      },
+    };
+
+    this.elements = {};
+    Object.keys(this.options.selectors).forEach((key) => {
+      this.elements[key] = this.querySelectorAll(this.options.selectors[key]);
+    });
+
+    this.runGate();
+  }
+
+  static get observedAttributes() {
+    return ['data-address'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.options.address = newValue;
+    // this.runGate();
+  }
+
+  set address(address) {
+    this.setAttribute('data-address', address);
+  }
+
+  runGate() {
+    this.options.address.forEach((symbole, index) => {
+      console.log(symbole);
+      this.elements.symboles
+        .style.transform = `rotate(${symbole * 10}deg)`;
+      this.activateHook(index);
+    });
+  }
+
+  activateHook(hook) {
+    console.log(hook);
   }
 }
 
