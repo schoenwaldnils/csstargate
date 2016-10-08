04efe5a3b6a86979b7fa62687474a958e62a50be
Nils Schönwald <nils@schoenwald.media>
2016-10-08 02:23:39 +0200

    Fix travis again

--- deploy.sh
+++ deploy.sh
@@ -20,13 +20,6 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing gh-pages for this repo into docs/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO docs
-cd docs
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
-
 # Clean out existing contents
 rm -rf docs/**/* || exit 0
 
